e746d33ca401f8df88abaa6139fc213620e66237
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-11-10 10:22:04 -0500

    part 1 completed

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,7 @@
-business = {
+import React from 'react';
+import './Business.css';
+
+const business = {
 	imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
 	name: 'MarginOtto Pizzeria',
 	address: '1010 Paddington Way',
@@ -9,3 +12,30 @@ business = {
 	rating: 4.5,
 	reviewCount: 90
 };
+
+class Business extends React.Component {
+	render() {
+		return (
+			<div className="Business">
+			  <div className="image-container">
+			    <img src={business.imageSrc} alt=''/>
+			  </div>
+			  <h2>{business.name}</h2>
+			  <div className="Business-information">
+			    <div className="Business-address">
+			      <p>{business.address}</p>
+			      <p>{business.city}</p>
+			      <p>{business.state} {business.zipCode}</p>
+			    </div>
+			    <div className="Business-reviews">
+			      <h3>{business.category}</h3>
+			      <h3 className="rating">{business.rating} stars</h3>
+			      <p>{business.reviewCount} reviews</p>
+			    </div>
+			  </div>
+			</div>
+		);
+	}
+}
+
+export default Business;
